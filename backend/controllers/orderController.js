621ac1505f178fbd2dd67322eeb5e4d17e81efd9
ponaganti saiteja <166588884+ponagantisaiteja@users.noreply.github.com>
2025-08-03 12:11:11 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // config variables
 const currency = "inr";
 const deliveryCharge = 50;
-const frontend_URL = 'https://tomato-frontend-ds0g.onrender.com';
+const frontend_URL = 'https://foodizo-frontend2.onrender.com';
 
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
